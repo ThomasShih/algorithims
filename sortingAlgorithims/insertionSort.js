f3f13caf5065ec84f6eec11f2642bfa5908c05f1
ThomasShih <55861476+ThomasShih@users.noreply.github.com>
2020-04-16 17:11:25 -0600

    insertion sort

---
+++ sortingAlgorithims/insertionSort.js
@@ -0,0 +1,28 @@
+function insert(inputArray,number){
+    //everything to the left should be sorted
+    for (var i = 0; i < inputArray.length;i++){
+        if(number <= inputArray[i]){
+            inputArray.splice(i,0,number)
+            return inputArray
+        }
+    }
+}
+
+function insertionSort(randomArray,ascending=true) {
+    for(var i = 1;i < randomArray.length;i++){
+        if(randomArray[i-1] > randomArray[i]){
+            insertionValue = randomArray.splice(i,1)[0]
+            randomArray = insert(randomArray,insertionValue)
+        }
+    }
+
+    if(!ascending){randomArray.reverse()}
+    return randomArray
+}
+
+if (require.main === module) {
+    var randomArray = [...Array(100)].map(() => Math.floor(Math.random() * 100));
+    console.log("Input of " + String(randomArray))
+    result = insertionSort(randomArray,ascending=true);
+    console.log("Output of " + String(result))
+}
