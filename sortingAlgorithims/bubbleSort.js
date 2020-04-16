7e26a6591b5bf3029281f8ec19afc233f97a1e06
ThomasShih <55861476+ThomasShih@users.noreply.github.com>
2020-04-16 16:23:17 -0600

    bubble sort

---
+++ sortingAlgorithims/bubbleSort.js
@@ -0,0 +1,33 @@
+function swapWithNext(inputArray,indexLoc){
+    firstItem = inputArray[indexLoc]
+    inputArray[indexLoc] = inputArray[indexLoc+1]
+    inputArray[indexLoc+1] = firstItem
+    return inputArray
+}
+
+function bubbleSort(randomArray,ascending=true) {
+    do{
+        finishPassing = true
+        for(var i = 0;i < randomArray.length;i++){
+            if(ascending){
+                if(randomArray[i] > randomArray[i+1]){
+                    randomArray = swapWithNext(randomArray,i)
+                    finishPassing = false
+                }
+            }else{
+                if(randomArray[i] < randomArray[i+1]){
+                    randomArray = swapWithNext(randomArray,i)
+                    finishPassing = false
+                }
+            }
+        }
+    }while(finishPassing == false)
+    return randomArray
+}
+
+if (require.main === module) {
+    var randomArray = [...Array(100)].map(() => Math.floor(Math.random() * 100));
+    console.log("Input of " + String(randomArray))
+    result = bubbleSort(randomArray,ascending=false);
+    console.log("Output of " + String(result))
+}
